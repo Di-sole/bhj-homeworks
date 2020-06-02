932bbb92f300ace75b9a3d056f07113815ff3bf5
Olga Abramovich <disole@bk.ru>
2020-06-03 00:35:22 +0500

    tasks fixed

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -9,11 +9,11 @@ function showTooltip(event) {
         this.nextElementSibling.classList.toggle('tooltip_active');
     } else {  
         const coords = this.getBoundingClientRect();
-        
+
         const tooltip = document.createElement('div');
         tooltip.className = 'tooltip tooltip_active';
-        tooltip.setAttribute('style', `left: ${coords.left}px; top: ${coords.bottom}px`)
-        tooltip.innerText = `${this.getAttribute('title')}`
+        tooltip.setAttribute('style', `left: ${coords.left}px; top: ${coords.bottom}px`);
+        tooltip.innerText = `${this.getAttribute('title')}`;
 
         this.insertAdjacentElement('afterEnd', tooltip);
     } 
